390836deb4d212093ec46e371a55401dc480261c
Tom Viner <tom@viner.tv>
2019-06-10 23:56:48 +0100

    add intro and pong

---
+++ pong/pong.py
@@ -0,0 +1,43 @@
+import sys
+
+HEIGHT = 600
+WIDTH = 900
+
+player_one = Rect((20, 20), (50, 200))
+player_two = Rect((WIDTH - 70, 20), (50, 200))
+
+ball = Rect((WIDTH / 2, HEIGHT / 2), (50, 50))
+ball_direction = [5, 15]
+
+def draw():
+    screen.fill((128, 0, 0))
+    screen.draw.filled_rect(player_one, (0, 128, 0))
+    screen.draw.filled_rect(player_two, (0, 128, 0))
+    screen.draw.filled_rect(ball, (0, 128, 0))
+
+def update(unicode):
+    ball.move_ip(*ball_direction)
+
+    if ball.colliderect(player_one):
+        ball_direction[0] *= -1
+
+    if ball.colliderect(player_two):
+        ball_direction[0] *= -1
+
+    if not 0 < ball.y < HEIGHT:
+        ball_direction[1] *= -1
+
+def on_key_down(unicode):
+    jump = 50
+    if unicode == 'q':
+        player_one.y -= jump
+    elif unicode == 'a':
+        player_one.y += jump
+
+    elif unicode == 'w':
+        player_two.y -= jump
+    elif unicode == 's':
+        player_two.y += jump
+    else:
+        sys.exit(0)
+
