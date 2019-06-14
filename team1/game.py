4f7b22173b0c845afe202fc44a164a023117831b
Alex Archer <alex@touchsurgery.com>
2019-06-14 15:45:24 +0100

    First enemy moving

--- team1/game.py
+++ team1/game.py
@@ -5,15 +5,21 @@ ship = Actor('player_ship2_red')
 ship.left = 0
 ship.bottom = HEIGHT
 
+enemy = Actor('enemy_black2.png')
+enemy.left = 0
+enemy.top = 0
+
 def draw():
     # screen.fill((0, 0, 255))
     screen.blit('background', (0, 0))
     screen.blit('background', (0, 400))
     
     ship.draw()
+    enemy.draw()
 
 def update():
     if keyboard.left:
         ship.x -= 15
     if keyboard.right:
         ship.x += 15
+    enemy.y += 10
