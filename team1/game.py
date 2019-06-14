b33ce2d28b5815f94986b165a6437427d9e329ae
Alex Archer <alex@touchsurgery.com>
2019-06-14 15:30:06 +0100

    Start ship in bottom left

--- team1/game.py
+++ team1/game.py
@@ -1,2 +1,10 @@
-HEIGHT = 400
-WIDTH = 200
+HEIGHT = 800
+WIDTH = 400
+
+ship = Actor('player_ship2_red')
+
+def draw():
+    screen.fill((0, 0, 255))
+    ship.left = 0
+    ship.bottom = HEIGHT
+    ship.draw()
