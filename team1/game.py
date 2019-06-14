0eb476bd3b00fd285dd31638e6fb56cc2010f630
Alex Archer <alex@touchsurgery.com>
2019-06-14 15:41:15 +0100

    ship n back

--- team1/game.py
+++ team1/game.py
@@ -2,9 +2,18 @@ HEIGHT = 800
 WIDTH = 400
 
 ship = Actor('player_ship2_red')
+ship.left = 0
+ship.bottom = HEIGHT
 
 def draw():
-    screen.fill((0, 0, 255))
-    ship.left = 0
-    ship.bottom = HEIGHT
+    # screen.fill((0, 0, 255))
+    screen.blit('background', (0, 0))
+    screen.blit('background', (0, 400))
+    
     ship.draw()
+
+def update():
+    if keyboard.left:
+        ship.x -= 15
+    if keyboard.right:
+        ship.x += 15
